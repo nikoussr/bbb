56e1040d29f5626c41012997952f76bd84424bc2
Захар <zahar.gaskov@ya.ru>
2025-04-18 00:21:21 +0700

    jinja + static + templates Добавил хтмл, си_эс_эс, имаге Работал только в ads_service

--- ads_service/api/handlers.py
+++ ads_service/api/handlers.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends, Body
+from fastapi import APIRouter, Depends, Body, Request
 from sqlalchemy.ext.asyncio import AsyncSession
 from watchfiles import awatch
 
@@ -6,40 +6,51 @@ from ads_service.db.session import get_db
 from ads_service.api.models import Ads_sc, Category_sc, Dormitory_sc, AdUpdate_sc
 from ads_service.api.actions.ads import _create_new_ad, _create_new_category, _create_new_dormitory, _delete_dormitory, \
     _delete_ad, _delete_category, _update_ad
+from fastapi.responses import HTMLResponse
+from starlette.templating import Jinja2Templates
+templates = Jinja2Templates(directory="templates")
 
-user_router = APIRouter()
 
 
-@user_router.post("/", response_model=Ads_sc)
-async def create_user(body: Ads_sc, db: AsyncSession = Depends(get_db)):
+
+ads_router = APIRouter()
+
+
+@ads_router.post("/create_new_ad", response_model=Ads_sc)
+async def create_new_ad(body: Ads_sc, db: AsyncSession = Depends(get_db)):
     return await _create_new_ad(body, db)
 
 
-@user_router.post("/categories", response_model=Category_sc)
+@ads_router.post("/categories", response_model=Category_sc)
 async def create_category(body: Category_sc, db: AsyncSession = Depends(get_db)):
     return await _create_new_category(body, db)
 
 
-@user_router.post("/dormitories", response_model=Dormitory_sc)
+@ads_router.post("/dormitories", response_model=Dormitory_sc)
 async def create_dormitory(body: Dormitory_sc, db: AsyncSession = Depends(get_db)):
     return await _create_new_dormitory(body, db)
 
 
-@user_router.delete("/dormitories/{dormitory_id}", status_code=204)
+@ads_router.delete("/dormitories/{dormitory_id}", status_code=204)
 async def delete_dormitory(dormitory_id: int, db: AsyncSession = Depends(get_db)):
     return await _delete_dormitory(dormitory_id, db)
 
 
-@user_router.delete("/categories/{category_id}", status_code=204)
+@ads_router.delete("/categories/{category_id}", status_code=204)
 async def delete_category(category_id: int, db: AsyncSession = Depends(get_db)):
     return await _delete_category(category_id, db)
 
 
-@user_router.delete("/ads/{ad_id}", status_code=204)
+@ads_router.delete("/ads/{ad_id}", status_code=204)
 async def delete_ad(ad_id: int, db: AsyncSession = Depends(get_db)):
     return await _delete_ad(ad_id, db)
 
 
-@user_router.patch("/ads/{ad_id}", response_model=Ads_sc)
+@ads_router.patch("/ads/{ad_id}", response_model=Ads_sc)
 async def update_ad(ad_id: int, update: AdUpdate_sc = Body(...), db: AsyncSession = Depends(get_db)):
     return await _update_ad(ad_id, db, update)
+
+
+@ads_router.get("/", response_class=HTMLResponse)
+async def read_index(request: Request):
+    return templates.TemplateResponse("indexB.html", {"request": request})
--- ads_service/main.py
+++ ads_service/main.py
@@ -3,8 +3,9 @@ from fastapi import FastAPI, APIRouter
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from ads_service import settings
-from ads_service.api.handlers import user_router
+from ads_service.api.handlers import ads_router
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from fastapi.staticfiles import StaticFiles
 
 engine = create_async_engine(settings.REAL_DATABASE_URL, future=True, echo=True)
 
@@ -15,7 +16,9 @@ Base = declarative_base()
 app = FastAPI()
 main_api_router = APIRouter()
 
-main_api_router.include_router(user_router, prefix="/ads", tags=["ads"])
+app.mount("/ads/static", StaticFiles(directory="static"), name="static")
+
+main_api_router.include_router(ads_router, prefix="/ads", tags=["ads"])
 app.include_router(main_api_router)
 
 if __name__ == "__main__":
